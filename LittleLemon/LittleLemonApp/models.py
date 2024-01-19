2822dd56e7eb87de3e3d5e2059ccd70d57ad8654
Eric <ericgorza@gmail.com>
2024-01-18 20:30:12 -0800

    models created and registered

--- LittleLemon/LittleLemonApp/models.py
+++ LittleLemon/LittleLemonApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+    guests = models.SmallIntegerField(default=6)
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.name
