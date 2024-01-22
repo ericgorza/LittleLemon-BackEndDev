9cce1bcb8797bff8e604f7105496c7d416185089
Eric <ericgorza@gmail.com>
2024-01-22 00:32:07 -0800

    protected the booking view

--- LittleLemon/LittleLemonApp/urls.py
+++ LittleLemon/LittleLemonApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import sayHello, index, MenuItemView, SingleMenuItem
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('test', sayHello, name='sayHello'),
     path('', index, name='index'),
     path('menu/', MenuItemView.as_view()),
     path('menu/<int:pk>', SingleMenuItem.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
